9e47230b5c9056d44334d1f0501bea557ee71d9e
rahulalam31 <gaberialcramern31@outlook.com>
2024-07-19 22:01:57 +0530

    AbuseIp serviceprovider

--- src/AbuseIPServiceProvider.php
+++ src/AbuseIPServiceProvider.php
@@ -1,20 +1,25 @@
 <?php
 
-namespace rahulalam31\AbuseIp;
+namespace RahulAlam31\LaravelAbuseIp;
 
 use Illuminate\Support\ServiceProvider;
-use rahulalam31\AbuseIp\Console\Commands\UpdateAbuseIps;
+use RahulAlam31\LaravelAbuseIp\Console\Commands\UpdateAbuseIps;
+use RahulAlam31\LaravelAbuseIp\Middleware\AbuseIp;
 
-class AbuseIpServiceProvider extends ServiceProvider
+class AbuseIPServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config/abuse-ip.php' => config_path('abuse-ip.php'),
+            __DIR__.'/../config/abuse-ip.php' => config_path('abuse-ip.php'),
         ], 'config');
 
+        $this->publishes([
+            __DIR__.'/middleware/AbuseIp.php' => app_path('Http/Middleware/AbuseIp.php'),
+        ]);
+
         $this->mergeConfigFrom(
-            __DIR__.'/config/abuse-ip.php', 'abuse-ip'
+            __DIR__.'/../config/abuse-ip.php', 'abuse-ip'
         );
 
         if($this->app->runningInConsole()){
@@ -22,11 +27,21 @@ class AbuseIpServiceProvider extends ServiceProvider
                 UpdateAbuseIps::class,
             ]);
         }
+
+        // $this->app['router']->middleware('block.abuse_ip', AbuseIp::class);
+
+
+        // Register middleware
+    //     $router = $this->app->withMiddleware(function (Middleware $middleware) {
+    //         $middleware->append(EnsureTokenIsValid::class);
+    //    });
+    //     $router->middleware('abuse_ip', AbuseIp::class);
+
     }
 
     public function register()
     {
-        $this->app['router']->aliasMiddleware('block.spam.ip', \rahulalam31\AbuseIp\Middleware\AbuseIp::class);
+        // $this->app['router']->aliasMiddleware('block.abuse_ip', \RahulAlam31\LaravelAbuseIp\Middleware\AbuseIp::class);
     }
 
 }

97dab335380b8336157596ae7ffcc3b40acbe2a7
rahulalam31 <gaberialcramern31@outlook.com>
2024-07-19 17:42:18 +0530

    update abuse ip command

---
+++ src/Console/Commands/UpdateAbuseIps.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace rahulalam31\AbuseIp\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class UpdateAbuseIps extends Command
+{
+    protected $signature = 'abuseip:update';
+    protected $description = 'update the abuse ip list.';
+
+    public function handle()
+    {
+        $this->info('Fetching IP blockList...');
+
+        //fetch the IP blocklist
+        $ips = $this->fetchIpsFromSources(config('spamipblocker.source'));
+
+        if(!empty($ips)){
+
+            //save to ip.json
+            file_put_contents(config('abuse-ip.storage'), json_encode($ips, JSON_PRETTY_PRINT));
+
+            Cache::forever('abuse_ips', $ips);
+
+            $this->info('IP blocklist updated successfully');
+        } else {
+            $this->error('Failed to fetch IP blocklist');
+        }
+
+    }
+
+    private function fetchIpsFromSources(array $sources)
+    {
+        $ips = [];
+        foreach ($sources as $source) {
+            $response = Http::get($source);
+            if ($response->successful()) {
+                $sourceIps = array_filter(explode("\n", $response->body()));
+                $ips = array_merge($ips, $sourceIps);
+            } else {
+                $this->error("Failed to fetch from source: $source");
+            }
+        }
+        return array_unique($ips);
+    }
+}

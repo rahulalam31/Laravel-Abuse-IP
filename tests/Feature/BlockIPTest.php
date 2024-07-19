ef9f1cc2cc095bc0633a00815d35c809517d2b9f
rahulalam31 <gaberialcramern31@outlook.com>
2024-07-19 22:00:52 +0530

    test case wip

---
+++ tests/Feature/BlockIPTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RahulAlam31\LaravelAbuseIp\tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Orchestra\Testbench\TestCase;
+use RahulAlam31\LaravelAbuseIp\AbuseIPServiceProvider;
+use Illuminate\Support\Facades\Route;
+use RahulAlam31\LaravelAbuseIp\Middleware\AbuseIp;
+
+class BlockIPTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [AbuseIPServiceProvider::class];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Clear cache before running tests
+        Cache::flush();
+
+        Route::middleware(AbuseIp::class)->get('/test-route', function () {
+            return 'ok';
+        });
+
+        // Ensure the configuration is loaded
+        $this->app['config']->set('abuse-ip', include __DIR__ . '/../../config/abuse-ip.php');
+    }
+
+
+    public function testRequestFromBlockedIp()
+    {
+
+        // Add test IPs to the cache
+        // Cache::forever('abuse_ips', ['192.168.0.1', '10.0.0.1']);
+
+        // Simulate request from a blocked IP
+        $response = $this->withServerVariables(['REMOTE_ADDR' => '1.0.136.77'])->get('/test-route');
+        $response->assertStatus(403);
+        $response->assertSee('Your IP address has been blocked');
+    }
+
+    public function testRequestFromAllowedIp()
+    {
+        // Add test IPs to the cache
+        Cache::forever('abuse_ips', ['192.168.0.1', '10.0.0.1']);
+
+        // Simulate request from a non-blocked IP
+        $response = $this->withServerVariables(['REMOTE_ADDR' => '8.8.8.8'])->get('/test-route');
+        $response->assertStatus(200);
+        $response->assertSee('ok');
+    }
+
+}

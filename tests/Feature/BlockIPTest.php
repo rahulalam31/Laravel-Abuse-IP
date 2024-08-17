4eb08c992561fb13a3f4fc0ee72480a4273cf045
EriBloo <19932449+EriBloo@users.noreply.github.com>
2024-08-17 15:33:00 +0000

    Fix tests

--- tests/Feature/BlockIPTest.php
+++ tests/Feature/BlockIPTest.php
@@ -34,7 +34,7 @@ class BlockIPTest extends TestCase
     {
 
         // Add test IPs to the cache
-        // Cache::forever('abuse_ips', ['192.168.0.1', '10.0.0.1']);
+        Cache::forever('abuse_ips', ['192.168.0.1', '10.0.0.1', '1.0.136.77']);
 
         // Simulate request from a blocked IP
         $response = $this->withServerVariables(['REMOTE_ADDR' => '1.0.136.77'])->get('/test-route');
--- tests/Feature/UpdateAbuseIpTest.php
+++ tests/Feature/UpdateAbuseIpTest.php
@@ -17,13 +17,16 @@ class UpdateAbuseIpTest extends TestCase
 
     public function testUpdateAbuseIpCommand()
     {
-        config(['abuseip.source' => [
-            'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-all.ipv4',
-            'https://example.com/blocklist-with-comments.txt',
-        ]]);
+        config([
+            'abuseip.source' => [
+                'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-all.ipv4',
+                'https://example.com/another-ip-list.txt',
+                'https://example.com/blocklist-with-comments.txt',
+            ],
+        ]);
 
         Http::fake([
-            'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/master/ips.txt' => Http::response("192.168.0.1\n10.0.0.1\n"),
+            'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-all.ipv4' => Http::response("192.168.0.1\n10.0.0.1\n"),
             'https://example.com/another-ip-list.txt' => Http::response("172.16.0.1\n10.0.0.1\n127.0.0.1"),
             'https://example.com/blocklist-with-comments.txt' => Http::response("# Some Blocklist\n1.2.3.4 # brute-force\n5.6.7.8\n127.0.0.9"),
         ]);
@@ -37,23 +40,27 @@ class UpdateAbuseIpTest extends TestCase
         // Assert the cache contains the correct IPS
         $cachedIps = Cache::get('abuse_ips');
         $this->assertNotEmpty($cachedIps);
-        $this->assertContains('1.0.136.77', $cachedIps);
-        $this->assertContains('1.0.158.159', $cachedIps);
-        $this->assertContains('1.1.109.151', $cachedIps);
+        $this->assertContains('192.168.0.1', $cachedIps);
+        $this->assertContains('10.0.0.1', $cachedIps);
+        $this->assertContains('172.16.0.1', $cachedIps);
+        $this->assertContains('127.0.0.1', $cachedIps);
         $this->assertContains('1.2.3.4', $cachedIps);
         $this->assertContains('5.6.7.8', $cachedIps);
+        $this->assertContains('127.0.0.9', $cachedIps);
         $this->assertNotContains('# Some Blocklist', $cachedIps);
 
         // Check that ip.json file is updated
-        $ipjsonPath = config('abuseip.storage') ;
+        $ipjsonPath = config('abuseip.storage');
         $this->assertFileExists($ipjsonPath);
         $ipsfromFile = json_decode(file_get_contents($ipjsonPath), true);
         $this->assertNotEmpty($ipsfromFile);
-        $this->assertContains('1.0.136.77', $ipsfromFile);
-        $this->assertContains('1.0.158.159', $ipsfromFile);
-        $this->assertContains('1.1.109.151', $ipsfromFile);
+        $this->assertContains('192.168.0.1', $ipsfromFile);
+        $this->assertContains('10.0.0.1', $ipsfromFile);
+        $this->assertContains('172.16.0.1', $ipsfromFile);
+        $this->assertContains('127.0.0.1', $ipsfromFile);
         $this->assertContains('1.2.3.4', $ipsfromFile);
         $this->assertContains('5.6.7.8', $ipsfromFile);
+        $this->assertContains('127.0.0.9', $ipsfromFile);
         $this->assertNotContains('# Some Blocklist', $ipsfromFile);
     }
 }

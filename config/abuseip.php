540c29404c874a42d0895ad31bcfd9b0f61d41b1
rahulalam31 <gaberialcramern31@outlook.com>
2024-08-20 01:53:52 +0530

    optimized and added whitelist option

--- config/abuseip.php
+++ config/abuseip.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Cache;
-
 return [
 
     /*
@@ -17,15 +15,25 @@ return [
         // https://github.com/borestad/blocklist-abuseipdb
         // Do not use the abuseipdb-s100-all.ipv4. It is only exposed for statistical usage.
         // Recommended usage is the maximum 30 days or less to avoid false positives.
-        'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-30d.ipv4',
+        // 30 Days list
+        // 'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-30d.ipv4',
+        // 14 Days list
+        'https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-14d.ipv4'
     ],
 
-    'abuse_ips' => function () {
-        return Cache::get('abuse_ips', function () {
-            $path = config('abuseip.storage');
-            return file_exists($path) ? json_decode(file_get_contents($path), true) : [];
-        });
-    },
+    /*
+    |--------------------------------------------------------------------------
+    | IP Whitelist
+    |--------------------------------------------------------------------------
+    |
+    | The IP addresses listed here will bypass the blocking middleware.
+    | You can add IPs as strings, and they will be checked before blocking logic.
+    |
+    */
+    'whitelist' => [
+        '127.0.0.1', // Localhost example
+        // Add more IPs here...
+    ],
 
     'storage' => storage_path('framework/cache/abuseip.json'),
 ];

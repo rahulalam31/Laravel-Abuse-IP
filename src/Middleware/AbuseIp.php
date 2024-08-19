ff2a6e4517a79c940fb04f68df62a368cd697297
rahulalam31 <gaberialcramern31@outlook.com>
2024-08-20 01:53:36 +0530

    updated function and added whitelist feature

--- src/Middleware/AbuseIp.php
+++ src/Middleware/AbuseIp.php
@@ -1,18 +1,29 @@
 <?php
 
-namespace App\Http\Middleware;
-
+namespace RahulAlam31\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 
 class AbuseIp
 {
+    protected $whitelistedIPs;
+
+    public function __construct()
+    {
+        // Fetch the whitelist from config
+        $this->whitelistedIPs = config('abuseip.whitelist', []);
+    }
+
     public function handle(Request $request, Closure $next)
     {
-        $abuseip = config('abuseip.abuse_ips')();
 
-        if (in_array($request->ip(), $abuseip)) {
+        // Check if the IP is whitelisted
+        if (in_array($request->ip(), $this->whitelistedIPs)) {
+            return $next($request); // Allow request if IP is whitelisted
+        }
+
+        if (is_abused_ip($request->ip())) {
 
             // Log::info('Blocking IP: ' . $request->ip());
             return response('Your IP address has been blocked', 403);
